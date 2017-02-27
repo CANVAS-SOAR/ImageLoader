80ddf37748f33af8f8f0ca671d9ddddeba8b578f
djseeds <dseeds14@gmail.com>
2017-02-26 22:19:20 -0500

    Added option for batched image loads
    For large datasets, it may not be convenient to load
    all images at once. Added 'cached' option
    (default False), which forces a full load to RAM
    if one so chooses. Otherwise, will load in batches.

--- ImageLoader.py
+++ ImageLoader.py
@@ -13,7 +13,8 @@ class ImageLoader:
     # @param trainYPattern     Pattern matching training data labels
     # @param testXPattern      Pattern matching test data
     # @param testYPattern      Pattern matching test data labels
-    def __init__(self, imageSize=(1392,512), trainXPattern="./traindata/x/*", trainYPattern="./traindata/y/*", testXPattern="./testdata/x/*", testYPattern="./testdata/y/*"):
+    # @param cached            Whether to load all images into RAM at once
+    def __init__(self, imageSize=None, trainXPattern="./traindata/x/*", trainYPattern="./traindata/y/*", testXPattern="./testdata/x/*", testYPattern="./testdata/y/*", cached=False):
 
         self.imageSize = imageSize
         self.trainXPattern = trainXPattern
@@ -21,6 +22,8 @@ class ImageLoader:
         
         self.testXPattern = testXPattern
         self.testYPattern = testYPattern
+
+        self.cached=cached
 
         self.trainX = None
         self.trainY = None
@@ -32,13 +35,21 @@ class ImageLoader:
 
     # @brief                Loads training data
     def loadTrainData(self):
-        self.trainX = loadImagesFromPattern(self.imageSize, self.trainXPattern)
-        self.trainY = loadImagesFromPattern(self.imageSize, self.trainYPattern)
+        if(self.cached):
+            self.trainX = loadImagesFromPattern(self.imageSize, self.trainXPattern)
+            self.trainY = loadImagesFromPattern(self.imageSize, self.trainYPattern)
+        else:
+            self.trainX = np.array(glob(self.trainXPattern))
+            self.trainY = np.array(glob(self.trainYPattern))
 
     # @brief                Loads test data
     def loadTestData(self):
-        self.testX = loadImagesFromPattern(self.imageSize, self.testXPattern)
-        self.testY = loadImagesFromPattern(self.imageSize, self.testYPattern)
+        if(self.cached):
+            self.testX = loadImagesFromPattern(self.imageSize, self.testXPattern)
+            self.testY = loadImagesFromPattern(self.imageSize, self.testYPattern)
+        else:
+            self.testX = np.array(glob(self.testXPattern))
+            self.testY = np.array(glob(self.testYPattern))
 
     # @brief                Returns a batch of size training samples
     #
@@ -56,16 +67,32 @@ class ImageLoader:
             else:
                 ind = range(self.batchIndex, self.batchIndex + size)
                 self.batchIndex = (self.batchIndex + size) % self.trainX.shape[0]
-            batch_X = self.trainX[[i % self.trainX.shape[0] for i in ind]]
-            batch_Y = self.trainY[[i % self.trainY.shape[0] for i in ind]]
+            if(self.cached):
+                batch_X = self.trainX[[i % self.trainX.shape[0] for i in ind]]
+                batch_Y = self.trainY[[i % self.trainY.shape[0] for i in ind]]
+            else:
+                batch_X_files = self.trainX[[i % self.trainX.shape[0] for i in ind]]
+                batch_Y_files = self.trainY[[i % self.trainY.shape[0] for i in ind]]
+                batch_X = loadImagesFromList(self.imageSize, batch_X_files)
+                batch_Y = loadImagesFromList(self.imageSize, batch_Y_files)
         return batch_X, batch_Y
 
     # @brief                Returns matrix containing test data
     #
+    # @param size           Max number of examples to return
+    #
     # @return X             Matrix containing test examples
-    def getTestData(self):
-        return self.testX, self.testY
-
+    # @return Y             Matrix containing test labels
+    def getTestData(self, size):
+        if(self.cached):
+            X = self.testX[0:max(size,self.testX.shape[0])]
+            Y = self.testY[0:max(size,self.testY.shape[0])]
+        else:
+            X = loadImagesFromList(self.imageSize, self.testX[0:min(size,self.testX.shape[0])])
+            Y = loadImagesFromList(self.imageSize, self.testY[0:min(size,self.testY.shape[0])])
+        self.testX = np.delete(self.testX, range(0, min(size, self.testX.shape[0])), 0)
+        self.testY = np.delete(self.testY, range(0, min(size, self.testY.shape[0])), 0)
+        return X, Y
 
 
 # @brief                Sets working directory to path
@@ -75,15 +102,26 @@ def setPath(self, path):
     if os.path.isdir(path):
         os.chdir(path)
 
+# @brief                Loads all images from a list of filenames
+#
+# @param imageSize      If not none, images will be resized to this size
+# @param filenames      List of filenames
+def loadImagesFromList(imageSize=None, filenames=[]):
+    data = None
+    if(len(filenames) != 0):
+        if(imageSize is not None):
+            data = np.stack([np.array(Image.open(f).resize(imageSize, Image.NEAREST)) for f in filenames])
+        else:
+            data = np.stack([np.array(Image.open(f)) for f in filenames])
+    return data
+
 # @brief                Loads all images that match a pattern, relative to current working directory
 # 
 # @param imageSize      images will be resized to this size
 # @param pattern        List of patterns to match filenames
 def loadImagesFromPattern(imageSize, pattern="./*"):
-    data = None
     files = glob(pattern)
-    data = np.stack([np.array(Image.open(f).resize(imageSize, Image.NEAREST)) for f in files])
-    return data
+    return loadImagesFromList(imageSize, files)
 
 # @brief                Loads all images from a given directory
 # 
